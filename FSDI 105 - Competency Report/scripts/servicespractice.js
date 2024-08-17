ce8e433f561afba256cf2abfaf1fd16fb8ab0dc9
Cadence1988 <ryan.marlow@sdgku.edu>
2024-08-16 19:43:40 -0700

    first commit

---
+++ FSDI 105 - Competency Report/scripts/servicespractice.js
@@ -0,0 +1,52 @@
+// constructor 
+function Service(title,price){
+    this.title=title;
+    this.price=price;
+}
+//use jquery
+function register(){
+    let title = $("#txtTitle").val();
+    let price = $('#txtPrice').val();
+
+    let service = new Service(title,price);
+    save(service);
+    $('input').serviceval('');//clear the inputs
+}
+
+function init(){
+    //hook events
+    $('#btnRegister').on();
+}
+
+window.onload=init;
+
+/////////////////////
+function Service(description, price) {
+    this.description = description;
+    this.price = price;
+}
+///////////////////////
+$(document).ready(function() {
+    $('#serviceForm').on('submit', function(event) {
+        event.preventDefault();
+        var description = $('#description').val();
+        var price = $('#price').val();
+        
+        if (description === '' || price === '') {
+            $('#notification').text('Please fill out all fields.').show();
+        } else {
+            $('#notification').text('Service registered successfully!').show();
+            $(this).trigger('reset'); // Clear form fields
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#description').on('change', function() {
+        // Get the selected option's price
+        var price = $(this).find('option:selected').data('price');
+        // Set the price input value
+        $('#price').val(price);
+    });
+});
+
